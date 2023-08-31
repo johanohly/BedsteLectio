edd88b0f688129ea79892411c2c7fd9ea9666853
Johnny JTH <johanohly@gmail.com>
2023-08-31 21:42:31 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    const response = await fetch("/api/log-ind", {
+      headers: {
+        username: "jooh2",
+        password: "NKMyWSeJ",
+        school: "681",
+      },
+    });
+    const data = await response.text();
+    console.log(data);
+  });
+</script>
+
+<h1>Hello World!</h1>
---
+++ src/routes/modul/[id]/+server.ts
@@ -0,0 +1,6 @@
+import type { RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({params}) => {
+    
+    return new Response();
+};
