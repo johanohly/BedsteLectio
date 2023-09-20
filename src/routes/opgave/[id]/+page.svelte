59c38aa1ec99c1e581c41b823e12e6b09ec4f505
JohnnyJTH <johanohly@gmail.com>
2023-09-20 15:19:00 +0200

    Assignment: handle documents with only note

--- src/routes/opgave/[id]/+page.svelte
+++ src/routes/opgave/[id]/+page.svelte
@@ -2,12 +2,7 @@
   import type { Assignment, RawAssignment } from "$lib/types/assignments";
 
   import { RequestData } from "$components";
-  import {
-    Card,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-  } from "$components/ui/card";
+  import { Card, CardDescription, CardHeader, CardTitle } from "$components/ui/card";
   import { Skeleton } from "$components/ui/skeleton";
   import { authStore } from "$lib/stores";
   import { decodeUserID } from "$lib/utilities/cookie";
@@ -27,35 +22,34 @@
     assignment = {
       billedTime: assignmentData.oplysninger.elevtid,
       class: assignmentData.oplysninger.hold,
-      date: DateTime.fromFormat(
-        assignmentData.oplysninger.afleveringsfrist,
-        "d/M-yyyy HH:mm",
-        {
-          locale: "da",
-        }
-      ),
+      date: DateTime.fromFormat(assignmentData.oplysninger.afleveringsfrist, "d/M-yyyy HH:mm", {
+        locale: "da",
+      }),
       description: assignmentData.oplysninger.opgavenote,
-      details:
-        assignmentData.oplysninger.opgavebeskrivelse?.replace(")", ")<br>") ??
-        "",
-      documents: assignmentData.opgave_indlæg.map((document) => ({
-        date: DateTime.fromFormat(document.tidspunkt, "d/M-yyyy HH:mm", {
-          locale: "da",
-        }),
-        name: document.dokument.match(/\[(.*?)\]/)[1],
-        url: document.dokument.match(/\((.*?)\)/)[1],
-        user: {
-          id: document.bruger.bruger_id,
-          name: document.bruger.navn,
-        },
-      })),
+      details: assignmentData.oplysninger.opgavebeskrivelse?.replace(")", ")<br>") ?? "",
+      documents: assignmentData.opgave_indlæg.map((document) => {
+        const nameMatches = document.dokument.match(/\[(.*?)\]/);
+        const name = nameMatches ? nameMatches[1] : document.indlæg;
+        const urlMatches = document.dokument.match(/\((.*?)\)/);
+        const url = urlMatches ? urlMatches[1] : "#";
+
+        return {
+          date: DateTime.fromFormat(document.tidspunkt, "d/M-yyyy HH:mm", {
+            locale: "da",
+          }),
+          name,
+          url,
+          user: {
+            id: document.bruger.bruger_id,
+            name: document.bruger.navn,
+          },
+        };
+      }),
       participants: assignmentData.gruppemedlemmer.map((participant) => ({
         id: participant.bruger_id,
         name: participant.navn,
       })),
-      status: assignmentData.afleveres_af.status_fravær
-        .toLowerCase()
-        .replace("aflev.", "afleveret "),
+      status: assignmentData.afleveres_af.status_fravær.toLowerCase().replace("aflev.", "afleveret "),
       title: assignmentData.oplysninger.opgavetitel,
     };
   }
@@ -92,16 +86,7 @@
     <section>
       <div class="flex flex-col-reverse md:flex-row items-start md:items-center justify-between">
         <h1 class="!mb-0">{assignment.title} ({assignment.class})</h1>
-        <a
-          class="flex items-center h-8 px-3 rounded-[6px] no-underline bg-dark hover:bg-dark-hover dark:bg-light dark:hover:bg-light-hover text-white dark:text-black"
-          href={`https://www.lectio.dk/lectio/${
-            $authStore.school
-          }/ElevAflevering.aspx?exerciseid=${data.id}&elevid=${decodeUserID(
-            $authStore.cookie
-          )}`}
-          target="_blank"
-          >Lectio <ExternalLink class="ml-2 h-4 w-4" /></a
-        >
+        <a class="flex items-center h-8 px-3 rounded-[6px] no-underline bg-dark hover:bg-dark-hover dark:bg-light dark:hover:bg-light-hover text-white dark:text-black" href={`https://www.lectio.dk/lectio/${$authStore.school}/ElevAflevering.aspx?exerciseid=${data.id}&elevid=${decodeUserID($authStore.cookie)}`} target="_blank">Lectio <ExternalLink class="ml-2 h-4 w-4" /></a>
       </div>
       <p class="!mt-0 !mb-0">{assignment.description}</p>
       {#if assignment.details}
@@ -137,7 +122,7 @@
             <Card class="mb-4">
               <CardHeader>
                 <CardTitle class="flex items-center"
-                  >{document.name}<Download class="ml-2" size="20" /></CardTitle
+                  >{document.name}{#if document.url !== "#"}<Download class="ml-2" size="20" />{/if}</CardTitle
                 >
                 <CardDescription>
                   {document.user.name}
