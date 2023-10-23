1b2197412d11e64e0773e23c5b18a5804f2ed2f0
JohnnyJTH <johanohly@gmail.com>
2023-10-23 15:33:05 +0200

    WIP answer forms

--- src/lib/types/forms.ts
+++ src/lib/types/forms.ts
@@ -15,3 +15,43 @@ export type SimpleForm = {
     owner: string;
     id: string;
 }
+
+export type RawForm = {
+    titel: string;
+    afstender: {
+        navn: string;
+        id: string;
+    };
+    anonym: boolean;
+    indhold: {
+        svar: {
+            type: null | "radio" | "checkbox" | "tekstfelt";
+            id: string;
+            muligheder: {
+                id: string;
+                tekst: string;
+            }[];
+        };
+        titel: string;
+        tekst: string;
+    }[];
+}
+
+export type Form = {
+    title: string;
+    owner: {
+        name: string;
+        id: string;
+    };
+    anonymous: boolean;
+    questions: {
+        type: null | "radio" | "checkbox" | "tekstfelt";
+        title: string;
+        description: string;
+        id: string;
+        options: {
+            id: string;
+            value: string;
+        }[];
+    }[];
+}
--- src/routes/spørgeskema/[id]/+page.svelte
+++ src/routes/spørgeskema/[id]/+page.svelte
@@ -1,7 +1,70 @@
 <script lang="ts">
+  import { RequestData } from "$components";
+  import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "$components/ui/card";
+  import { Skeleton } from "$components/ui/skeleton";
+  import type { Form, RawForm } from "$lib/types/forms";
   import type { PageData } from "./$types";
 
   export let data: PageData;
+
+  let loading = true;
+  let formData: RawForm;
+  let form: Form;
+  let response: Record<string, string> = {};
+  $: if (!loading && formData) {
+    form = {
+      title: formData.titel,
+      owner: {
+        name: formData.afstender.navn,
+        id: formData.afstender.id,
+      },
+      anonymous: formData.anonym,
+      questions: formData.indhold.map((q) => ({
+        type: q.svar.type,
+        title: q.titel,
+        description: q.tekst,
+        id: q.svar.id,
+        options: q.svar.muligheder.map((o) => ({
+          id: o.id,
+          value: o.tekst,
+        })),
+      })),
+    };
+  }
+
+  const sendResponse = async () => {
+    const resp = await fetch(`/besvar_spoergeskema?id=${data.id}`, {
+      method: "POST",
+      body: JSON.stringify(response),
+    });
+  };
 </script>
 
-{data.id}
+<RequestData bind:loading bind:data={formData} path={`spoergeskema?id=${data.id}`} />
+
+<div class="page-container">
+  {#if loading}
+    <Skeleton class="h-[2.25em] w-3/4" />
+  {:else}
+    <h1>{form.title}</h1>
+    {#each form.questions as question}
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            {question.title}
+          </CardTitle>
+          {#if question.description}
+            <CardDescription>
+              {question.description}
+            </CardDescription>
+          {/if}
+        </CardHeader>
+        <CardContent>
+          {#if question.type === "tekstfelt"}
+            <textarea class="w-full border" placeholder="Skriv dit svar her..." bind:value={response[question.id]} />
+          {/if}
+        </CardContent>
+      </Card>
+    {/each}
+  {/if}
+</div>
