2f84611656fbbfa221750e3aff272b7d84f87eea
JohnnyJTH <johanohly@gmail.com>
2023-10-27 09:37:13 +0200

    Fix decodeUserID

--- src/lib/utilities/cookie.ts
+++ src/lib/utilities/cookie.ts
@@ -1,8 +1,11 @@
 export const decodeUserID = (cookie: string): string => {
     try {
-        const data = JSON.parse(Buffer.from(cookie, "base64").toString());
+        let data = []
+        if (window) JSON.parse(window.atob(cookie))
+        else data = JSON.parse(Buffer.from(cookie, "base64").toString());
         return data.filter((item) => item.name == "LastLoginElevId")[0].value
     } catch (error) {
+        console.log(error);
         return "54443227086"; // My user id xD
     }
 }
