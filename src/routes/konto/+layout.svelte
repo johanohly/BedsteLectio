d4c85031938508cffa3f7de08fd6689ad20c6ff5
JohnnyJTH <johanohly@gmail.com>
2023-10-31 09:38:52 +0100

    Mobile-friendly settings

--- src/routes/konto/+layout.svelte
+++ src/routes/konto/+layout.svelte
@@ -5,16 +5,14 @@
 </script>
 
 <div class="page-container">
-  <h1>Min Konto</h1>
-  <div class="flex flex-1 justify-start items-stretch">
-    <div class="not-prose flex flex-[0_0_auto]">
-      <div class="sticky mr-2 h-16 w-44">
-        <a class="block py-2 px-3 ml-[-12px] text-[14px] rounded-[5px] transition-link text-[#7d7d7d] hover:bg-light-hover hover:text-black dark:hover:bg-dark-hover dark:hover:text-white {$page.url.pathname.endsWith('/konto/generel') ? activeStyles : ''}" href="/konto/generel">Generel</a>
-        <a class="block py-2 px-3 ml-[-12px] text-[14px] rounded-[5px] transition-link text-[#7d7d7d] hover:bg-light-hover hover:text-black dark:hover:bg-dark-hover dark:hover:text-white {$page.url.pathname.endsWith('/konto/udseende') ? activeStyles : ''}" href="/konto/udseende">Udseende</a>
-        <a class="block py-2 px-3 ml-[-12px] text-[14px] rounded-[5px] transition-link text-[#7d7d7d] hover:bg-light-hover hover:text-black dark:hover:bg-dark-hover dark:hover:text-white {$page.url.pathname.endsWith('/konto/termin') ? activeStyles : ''}" href="/konto/termin">Termin</a>
-      </div>
+  <h1 class="mb-0">Min Konto</h1>
+  <div class="!mt-4 flex justify-start items-stretch flex-col md:flex-row">
+    <div class="not-prose flex-row md:flex-col md:w-44">
+      <a class="md:block py-2 px-3 ml-[-12px] text-[14px] rounded-[5px] transition-link text-[#7d7d7d] hover:bg-light-hover hover:text-black dark:hover:bg-dark-hover dark:hover:text-white {$page.url.pathname.endsWith('/konto/generel') ? activeStyles : ''}" href="/konto/generel">Generel</a>
+      <a class="md:block py-2 px-3 ml-[-12px] text-[14px] rounded-[5px] transition-link text-[#7d7d7d] hover:bg-light-hover hover:text-black dark:hover:bg-dark-hover dark:hover:text-white {$page.url.pathname.endsWith('/konto/udseende') ? activeStyles : ''}" href="/konto/udseende">Udseende</a>
+      <a class="md:block py-2 px-3 ml-[-12px] text-[14px] rounded-[5px] transition-link text-[#7d7d7d] hover:bg-light-hover hover:text-black dark:hover:bg-dark-hover dark:hover:text-white {$page.url.pathname.endsWith('/konto/termin') ? activeStyles : ''}" href="/konto/termin">Termin</a>
     </div>
-    <div class="flex flex-1 ml-2">
+    <div class="flex flex-1 max-md:mt-4 lg:ml-4">
       <main class="w-full">
         <slot />
       </main>
