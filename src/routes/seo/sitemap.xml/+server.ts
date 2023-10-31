4a75ca4a160449d359cdf9468df946772495bbc2
JohnnyJTH <johanohly@gmail.com>
2023-10-31 13:52:30 +0100

    Fix sitemap

--- src/routes/seo/sitemap.xml/+server.ts
+++ src/routes/seo/sitemap.xml/+server.ts
@@ -3,6 +3,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
     return await sitemap.response({
-        origin: 'https://bedstelectio.tech'
+        origin: 'https://bedstelectio.tech',
+        excludePatterns: ["/modul", "/opgave", "/spørgeskema"]
     });
 };
