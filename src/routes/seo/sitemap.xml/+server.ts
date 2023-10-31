d4af5246e9b6192101b113b5c0441c1951de2696
JohnnyJTH <johanohly@gmail.com>
2023-10-31 13:49:24 +0100

    Add sitemap

---
+++ src/routes/seo/sitemap.xml/+server.ts
@@ -0,0 +1,8 @@
+import * as sitemap from 'super-sitemap';
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async () => {
+    return await sitemap.response({
+        origin: 'https://bedstelectio.tech'
+    });
+};
