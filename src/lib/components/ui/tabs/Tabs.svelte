da2f22be83c21896bcf3cdaeafd18936da533189
JohnnyJTH <johanohly@gmail.com>
2023-11-02 08:48:45 +0100

    Responsive tabs

--- src/lib/components/ui/tabs/Tabs.svelte
+++ src/lib/components/ui/tabs/Tabs.svelte
@@ -18,8 +18,7 @@
   const hoveredTabIndex: Writable<null | number> = writable(null);
   const selectedTabIndex = writable(0);
 
-  $: selectedRect =
-    $buttonRefs[$selectedTabIndex]?.getBoundingClientRect() ?? null;
+  $: selectedRect = $buttonRefs[$selectedTabIndex]?.getBoundingClientRect() ?? null;
   const buttonRefs: Writable<Array<HTMLButtonElement>> = writable([]);
 
   const isInitialHoveredElement = writable(true);
@@ -40,7 +39,11 @@
 
       return i;
     });
-    hoveredRect.set(e.target.getBoundingClientRect());
+
+    // Calculate the scroll position and set the hoveredRect
+    const rect = e.target.getBoundingClientRect();
+    const scrollY = window.scrollY;
+    hoveredRect.set(new DOMRect(rect.x, rect.y + scrollY, rect.width, rect.height));
   };
 
   const onSelectTab = (i: number) => {
@@ -49,62 +52,30 @@
 
   let hoverStyles = "opacity: 0;";
   $: if (navRect && $hoveredRect) {
-    hoverStyles = `transform: translate3d(${
-      $hoveredRect.left - navRect.left
-    }px,${$hoveredRect.top - navRect.top}px,0px);`;
+    hoverStyles = `transform: translate3d(${$hoveredRect.left - navRect.left}px,${$hoveredRect.top - navRect.top}px,0px);`;
     hoverStyles += `width: ${$hoveredRect.width}px;`;
     hoverStyles += `height: ${$hoveredRect.height}px;`;
     hoverStyles += `opacity: ${$hoveredTabIndex != null ? 1 : 0};`;
-    hoverStyles +=
-      "transition: " +
-      ($isInitialHoveredElement
-        ? `opacity 150ms;`
-        : `transform 150ms 0ms, opacity 150ms 0ms, width 150ms;`);
+    hoverStyles += "transition: " + ($isInitialHoveredElement ? `opacity 150ms;` : `transform 150ms 0ms, opacity 150ms 0ms, width 150ms;`);
   }
 
   let selectStyles = "opacity: 0;";
   $: if (navRect && selectedRect) {
     selectStyles = `width: ${selectedRect.width * 0.8}px;`;
-    selectStyles += `transform: translateX(calc(${
-      selectedRect.left - navRect.left
-    }px + 10%));`;
+    selectStyles += `transform: translateX(calc(${selectedRect.left - navRect.left}px + 10%));`;
     selectStyles += "opacity: 1;";
-    selectStyles +=
-      "transition: " +
-      (isInitialRender
-        ? `opacity 150ms 150ms;`
-        : `transform 150ms 0ms, opacity 150ms 150ms, width 150ms;`);
+    selectStyles += "transition: " + (isInitialRender ? `opacity 150ms 150ms;` : `transform 150ms 0ms, opacity 150ms 150ms, width 150ms;`);
 
     isInitialRender = false;
   }
 </script>
 
-<nav
-  bind:this={navRef}
-  class="flex flex-shrink-0 items-center relative z-0 py-2"
-  on:pointerleave={onLeaveTabs}
->
+<nav bind:this={navRef} class="flex flex-shrink-0 items-center relative z-0 py-2" on:pointerleave={onLeaveTabs}>
   {#each tabs as tab, i}
-    <button
-      bind:this={$buttonRefs[i]}
-      class="text-md relative rounded-md flex items-center h-8 px-4 z-20 bg-transparent text-base text-slate-500 dark:text-slate-400 cursor-pointer select-none transition-colors {$hoveredTabIndex ===
-        i || $selectedTabIndex === i
-        ? '!text-black dark:!text-white'
-        : ''}"
-      id={String(i)}
-      on:click={() => onSelectTab(i)}
-      on:focus={(e) => onEnterTab(e, i)}
-      on:pointerenter={(e) => onEnterTab(e, i)}
-    >
+    <button bind:this={$buttonRefs[i]} class="text-md relative rounded-md flex items-center h-8 px-4 z-20 bg-transparent text-base text-slate-500 dark:text-slate-400 cursor-pointer select-none transition-colors {$hoveredTabIndex === i || $selectedTabIndex === i ? '!text-black dark:!text-white' : ''}" id={String(i)} on:click={() => onSelectTab(i)} on:focus={(e) => onEnterTab(e, i)} on:pointerenter={(e) => onEnterTab(e, i)}>
       {tab.label}
     </button>
   {/each}
-  <div
-    class="hidden lg:block absolute z-10 top-0 left-0 rounded-md bg-gray-200 dark:bg-dark-hover transition-[width]"
-    style={hoverStyles}
-  />
-  <div
-    class="absolute z-10 bottom-0 left-0 h-0.5 bg-black dark:bg-white"
-    style={selectStyles}
-  />
+  <div class="hidden lg:block absolute z-10 top-0 left-0 rounded-md bg-gray-200 dark:bg-dark-hover transition-[width]" style={hoverStyles} />
+  <div class="absolute z-10 bottom-0 left-0 h-0.5 bg-black dark:bg-white" style={selectStyles} />
 </nav>
