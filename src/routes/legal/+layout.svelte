8fbddce2686c4407f22ea75d2a19b68de15e0652
JohnnyJTH <johanohly@gmail.com>
2023-11-29 18:04:57 +0100

    Fix active tab border color

--- src/routes/legal/+layout.svelte
+++ src/routes/legal/+layout.svelte
@@ -4,7 +4,7 @@
     $: activeTab = decodeURI($page.url.pathname.split("/")[2] || "oversigt").toTitleCase();
 
     const cInactive = "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300";
-    const cActive = "inline-block p-4 border-b-2 text-black border-black dark:text-white";
+    const cActive = "inline-block p-4 border-b-2 text-black dark:text-white border-black dark:border-white";
 </script>
 
 <div class="not-prose container text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
