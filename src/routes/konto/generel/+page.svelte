f689cb61a76c3d73f29036fd7fbb316ee01e9b8b
JohnnyJTH <johanohly@gmail.com>
2023-11-29 20:59:39 +0100

    Fix indentation in import statement

--- src/routes/konto/generel/+page.svelte
+++ src/routes/konto/generel/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
+  import { goto } from "$app/navigation";
   import { addToast } from "$components/toaster";
   import { Confirm } from "$components/ui/confirm";
   import { authStore } from "$lib/stores";
