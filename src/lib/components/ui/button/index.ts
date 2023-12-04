c26ca9cb4658c06562acd97a397e0deec0c38271
JohnnyJTH <johanohly@gmail.com>
2023-12-04 11:14:48 +0100

    Add "xl" buttonVariant

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -3,7 +3,7 @@ import { cva } from "class-variance-authority";
 export { default as Button } from "./Button.svelte";
 
 export const buttonVariants = cva(
-	"inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed ring-offset-background",
+	"inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors no-underline focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed ring-offset-background",
 	{
 		defaultVariants: {
 			size: "default",
@@ -14,7 +14,8 @@ export const buttonVariants = cva(
 				default: "h-10 py-2 px-4",
 				lg: "h-11 px-8 rounded-md",
 				sm: "h-8 px-3 rounded-[6px]",
-				icon: "h-11 w-11 rounded-[6px]"
+				icon: "h-11 w-11 rounded-[6px]",
+				xl: "h-20 p-8 rounded-md text-2xl font-mono"
 			},
 			variant: {
 				default:
