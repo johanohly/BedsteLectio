018e22ba9ccff5372392694119ecda7279d95122
JohnnyJTH <johanohly@gmail.com>
2023-12-13 08:33:17 +0100

    Handle intervals between two different dates

--- src/lib/utilities/interval.ts
+++ src/lib/utilities/interval.ts
@@ -2,13 +2,18 @@ import { DateTime, Interval } from "luxon";
 
 export const constructInterval = (input: string) => {
     const parts = input.split(" ");
-    if (parts.length !== 4) {
+    let fromDate, fromTime, toDate, toTime;
+    if (parts.length === 4) {
+        [fromDate, fromTime, , toTime] = parts;
+        toDate = fromDate
+    } else if (parts.length === 5) {
+        [fromDate, fromTime, , toDate, toTime] = parts;
+    } else {
         return Interval.invalid("Invalid input string for interval");
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [date, fromTime, _, toTime] = parts;
-    const from = DateTime.fromFormat(`${date} ${fromTime}`, "d/M-yyyy HH:mm", { locale: "da" });
-    const to = DateTime.fromFormat(`${date} ${toTime}`, "d/M-yyyy HH:mm", { locale: "da" });
+    const from = DateTime.fromFormat(`${fromDate} ${fromTime}`, "d/M-yyyy HH:mm", { locale: "da" });
+    const to = DateTime.fromFormat(`${toDate} ${toTime}`, "d/M-yyyy HH:mm", { locale: "da" });
     return Interval.fromDateTimes(from, to)
 };
